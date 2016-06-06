477a0c74bc4698cef683344c524606c8ee8f2d45
Arfulmcdarful <blahaha>
2016-06-06 09:53:19 -0600

    Added New Zealand Info

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,7 @@ public class Main extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btnFlag1 = new JButton("");
 		btnFlag1.addMouseListener(new MouseAdapter() {
 			@Override
@@ -58,7 +58,7 @@ public class Main extends JFrame {
 		btnFlag1.setIcon(new ImageIcon("res/canada.png"));
 		btnFlag1.setBounds(10, 11, 60, 40);
 		contentPane.add(btnFlag1);
-		
+
 		JButton btnFlag2 = new JButton("");
 		btnFlag2.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
@@ -68,15 +68,15 @@ public class Main extends JFrame {
 		btnFlag2.setIcon(new ImageIcon("res/kyrgyzstan.png"));
 		btnFlag2.setBounds(80, 11, 60, 40);
 		contentPane.add(btnFlag2);
-		
+
 		JButton btnFlag3 = new JButton("#3");
 		btnFlag3.setBounds(150, 11, 60, 40);
 		contentPane.add(btnFlag3);
-		
+
 		JButton btnFlag4 = new JButton("#4");
 		btnFlag4.setBounds(220, 11, 60, 40);
 		contentPane.add(btnFlag4);
-		
+
 		JButton btnFlag5 = new JButton("");
 		btnFlag5.addMouseListener(new MouseAdapter() {
 			@Override
@@ -87,31 +87,38 @@ public class Main extends JFrame {
 		btnFlag5.setIcon(new ImageIcon("res/niger.png"));
 		btnFlag5.setBounds(290, 11, 60, 40);
 		contentPane.add(btnFlag5);
-		
+
 		JButton btnFlag6 = new JButton("#6");
 		btnFlag6.setBounds(10, 62, 60, 40);
 		contentPane.add(btnFlag6);
-		
+
 		JButton btnFlag7 = new JButton("#7");
 		btnFlag7.setBounds(80, 62, 60, 40);
 		contentPane.add(btnFlag7);
-		
+
 		JButton btnFlag8 = new JButton("#8");
 		btnFlag8.setBounds(150, 62, 60, 40);
 		contentPane.add(btnFlag8);
-		
+
 		JButton btnFlag9 = new JButton("#9");
 		btnFlag9.setBounds(220, 62, 60, 40);
 		contentPane.add(btnFlag9);
-		
-		JButton btnFlag11 = new JButton("#11");
+
+		JButton btnFlag11 = new JButton("");
+		btnFlag11.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				btnFlag11_mouseClicked(arg0);
+			}
+		});
+		btnFlag11.setIcon(new ImageIcon("res/newzealand.png"));
 		btnFlag11.setBounds(10, 113, 60, 40);
 		contentPane.add(btnFlag11);
-		
+
 		JButton btnFlag12 = new JButton("#12");
 		btnFlag12.setBounds(80, 113, 60, 40);
 		contentPane.add(btnFlag12);
-		
+
 		JButton btnFlag13 = new JButton("");
 		btnFlag13.addMouseListener(new MouseAdapter() {
 			@Override
@@ -122,36 +129,36 @@ public class Main extends JFrame {
 		btnFlag13.setIcon(new ImageIcon("res/nigeria.png"));
 		btnFlag13.setBounds(150, 113, 60, 40);
 		contentPane.add(btnFlag13);
-		
+
 		JButton btnFlag14 = new JButton("#14");
 		btnFlag14.setBounds(220, 113, 60, 40);
 		contentPane.add(btnFlag14);
-		
+
 		JButton btnFlag16 = new JButton("#16");
 		btnFlag16.setBounds(10, 164, 60, 40);
 		contentPane.add(btnFlag16);
-		
+
 		JButton btnFlag17 = new JButton("#17");
 		btnFlag17.setBounds(80, 164, 60, 40);
 		contentPane.add(btnFlag17);
-		
+
 		JButton btnFlag18 = new JButton("#18");
 		btnFlag18.setBounds(150, 164, 60, 40);
 		contentPane.add(btnFlag18);
-		
+
 		JButton btnFlag19 = new JButton("#19");
 		btnFlag19.setBounds(220, 164, 60, 40);
 		contentPane.add(btnFlag19);
-		
+
 		JButton btnFlag10 = new JButton("#10");
 		btnFlag10.setBounds(290, 62, 60, 40);
 		contentPane.add(btnFlag10);
-		
+
 		JButton btnFlag15 = new JButton("#15");
 		btnFlag15.setBounds(290, 113, 60, 40);
 		contentPane.add(btnFlag15);
 		//
-		
+
 		JButton btnFlag20 = new JButton("");
 		btnFlag20.addMouseListener(new MouseAdapter() {
 			@Override
@@ -162,40 +169,40 @@ public class Main extends JFrame {
 		btnFlag20.setIcon(new ImageIcon("res/flag_fr.png"));
 		btnFlag20.setBounds(290, 164, 60, 40);
 		contentPane.add(btnFlag20);
-			
+
 		//
 		txtCountryName = new JTextField();
 		txtCountryName.setBounds(80, 234, 270, 27);
 		contentPane.add(txtCountryName);
 		txtCountryName.setColumns(10);
-		
+
 		txtCapital = new JTextField();
 		txtCapital.setColumns(10);
 		txtCapital.setBounds(80, 272, 270, 27);
 		contentPane.add(txtCapital);
-		
+
 		txtCurrency = new JTextField();
 		txtCurrency.setColumns(10);
 		txtCurrency.setBounds(80, 310, 270, 27);
 		contentPane.add(txtCurrency);
-		
+
 		txtLanguage = new JTextField();
 		txtLanguage.setColumns(10);
 		txtLanguage.setBounds(80, 348, 270, 27);
 		contentPane.add(txtLanguage);
-		
+
 		JLabel lblCountryName = new JLabel("Country");
 		lblCountryName.setBounds(10, 240, 60, 14);
 		contentPane.add(lblCountryName);
-		
+
 		JLabel lblCapital = new JLabel("Capital");
 		lblCapital.setBounds(10, 278, 60, 14);
 		contentPane.add(lblCapital);
-		
+
 		JLabel lblCurrency = new JLabel("Currency");
 		lblCurrency.setBounds(10, 316, 60, 14);
 		contentPane.add(lblCurrency);
-		
+
 		JLabel lblLanguages = new JLabel("Language");
 		lblLanguages.setBounds(10, 354, 60, 14);
 		contentPane.add(lblLanguages);
@@ -207,14 +214,14 @@ public class Main extends JFrame {
 		this.txtCurrency.setText("Dollar");
 		this.txtLanguage.setText("English, French");
 	}
-	
+
 	protected void btnFlag2_mouseClicked(MouseEvent arg0) {
 		this.txtCountryName.setText("Kyrgyzstan");
 		this.txtCapital.setText("Bishkek");
 		this.txtCurrency.setText("Kyrgyzstani som");
 		this.txtLanguage.setText("Kyrgyz, Russian");
 	}
-	
+
 	protected void do_btnFlag5_mouseClicked(MouseEvent arg0) {
 		this.txtCountryName.setText("Niger");
 		this.txtCapital.setText("Niamey");
@@ -222,12 +229,20 @@ public class Main extends JFrame {
 		this.txtLanguage.setText("French");
 	}
 	
+	protected void btnFlag11_mouseClicked(MouseEvent arg0) {
+		this.txtCountryName.setText("New Zealand");
+		this.txtCapital.setText("Wellington");
+		this.txtCurrency.setText("New Zealand Dollar");
+		this.txtLanguage.setText("Maori, New Zealand Sign Language");
+	}
+
 	protected void do_btnFlag13_mouseClicked(MouseEvent arg0) {
 		this.txtCountryName.setText("Nigeria");
 		this.txtCapital.setText("Abuja");
 		this.txtCurrency.setText("Naira");
 		this.txtLanguage.setText("English");
 	}
+
 	protected void do_btnFlag20_mouseClicked(MouseEvent arg0) {
 		this.txtCountryName.setText("France");
 		this.txtCapital.setText("Paris");
